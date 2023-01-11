28a3eb34d8a58b83d49bd72eea029462b1426490
kbinko <konradbinko.webdev@gmail.com>
2023-01-11 11:18:00 +0100

    final

--- script.js
+++ script.js
@@ -51,22 +51,132 @@
 //         });
 // });
 
-$(document).ready(function() {
-    $(".select-me")
-        .on("keyup", function() {
-            console.log($(this).val());
-        })
-        .on("focus", function() {
-            console.log("focus");
-        })
-        .on("blur", function() {
-            console.log("blur");
-        });
-});
+// $(document).ready(function() {
+//     $(".select-me")
+//         .on("keyup", function() {
+//             console.log($(this).val());
+//         })
+//         .on("focus", function() {
+//             console.log("focus");
+//         })
+//         .on("blur", function() {
+//             console.log("blur");
+//         });
+// });
+
+// $(document).ready(function() {
+//     // $("form").on("submit", function(e) {
+//     //     e.preventDefault();
+//     //     console.log("form submitted");
+//     // });
+//     $("button").on("click", function(e) {
+//         e.preventDefault();
+//         console.log("button does not submit");
+//     });
+// });
 
 $(document).ready(function() {
-    $("form").on("submit", function(e) {
+    $("#show").hide();
+    $("#hide").on("click", function(e) {
         e.preventDefault();
-        console.log("form submitted");
+        $("#box").hide()
+        $("#hide").hide()
+        $("#show").show()
+        $("#style").hide()
+        $("#remove").hide()
+        $("#toggle").hide()
+        $("#fade").hide()
+        $("#slide").hide()
+        $("#callback").hide()
+        $("#animate").hide()
+    });
+    $("#show").on("click", function(e) {
+        e.preventDefault();
+        $("#box").show()
+        $("#hide").show()
+        $("#show").hide()
+        $("#style").show()
+        $("#remove").show()
+        $("#toggle").show()
+        $("#fade").show()
+        $("#slide").show()
+        $("#callback").show()
+        $("#animate").show()
+    });
+    $("#style").on("click", function(e) {
+        e.preventDefault();
+        $("#box").addClass("red");
+    });
+    $("#remove").on("click", function(e) {
+        e.preventDefault();
+        $("#box").removeClass("red");
+    });
+    $("#toggle").on("click", (e) => {
+        e.preventDefault();
+        $("#box").toggleClass("red");
+    });
+    $("#fade").on("click", (e) => {
+        e.preventDefault();
+        $("#box").fadeToggle(2000);
+    });
+    $("#slide").on("click", (e) => {
+        e.preventDefault();
+        $("#box").slideToggle(2000);
+    });
+    $("#callback").on("click", (e) => {
+        e.preventDefault();
+        $("#box").fadeToggle(2000, function() {
+            console.log("Fade complete");
+        });
+    });
+    $("#animate").on("click", (e) => {
+        e.preventDefault();
+        $("#box").animate({
+            "width": "500px",
+            "height": "500px",
+            "opacity": "0.5"
+        }, 2000);
+    });
+
+    // $(window).on("resize", function() {
+    //     if ($(window).width() < 500) {
+    //         $("body").css("background", "red");
+    //     } else {
+    //         $("body").css("background", "white");
+    //     }
+    // });
+
+    // $("li").each(function(index, element) {
+    //     $(element).text("item " + index)
+    //     if (index === 2) {
+    //         $(element).css("color", "red")
+    //     } else {
+    //         $(element).css("color", "black")
+    //     }
+    // })
+    $("#text").attr("type", "password")
+
+    const random = Math.ceil(Math.random() * 88);
+
+    $.get('https://swapi.dev/api/people/1/', function(data) {
+        console.log(data);
+    });
+    $.post('https://httpbin.org/post', { name: 'John', time: '2pm' }, function(data) {
+        console.log("Post was complete with this data:" + data);
+    });
+
+    $("#generate").on("click", function(e) {
+        $.getJSON('https://swapi.dev/api/people/' + random + '/', function(data) {
+            console.log(data);
+            $("#name").text("imie: " + data.name);
+            $("#height").text("wzrost: " + data.height);
+            $("#hair_color").text("kolor włosów: " + data.hair_color);
+            if (data.mass === "unknown") {
+                $("#mass").text("waga: " + data.mass);
+            } else {
+                $("#mass").text("waga; " + data.mass + " kg");
+            }
+
+        });
     });
 });
